0ebd638f060209888d3b925ea12d329b0adfbfe4
mateo <aniampira@gmail.com>
2025-10-11 23:39:46 -0500

    Add Uvicorn server configuration and update main.py to run the FastAPI app on specified port

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+import uvicorn
 from fastapi import FastAPI
 from routes.students import students_router
 from routes.staff import staff_router
@@ -17,3 +19,7 @@ app.include_router(metrics_router)
 @app.get("/", tags=["test"])
 async def root():
     return {"message": "Hello World"}
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
